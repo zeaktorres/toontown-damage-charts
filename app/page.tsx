e072d65beac352ed02855fd1bad77e3025cbca6d
zeaktorres <torreszeaks@gmail.com>
2023-06-17 01:03:38 -0700

    testing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+'use client'
+import { useTheme, Text } from '@nextui-org/react';
+import { Button } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
+
 export default function Home() {
+
+  const { theme } = useTheme();
   return (
-    <div>Hello from neovim!</div>
+   <div className='content-center absolute top-10 left-10'>
+        <Button size="xl">Simulate Fight </Button>       
+        <Image
+            width={320}
+            height={180}  
+            src="../docs/assets/images/Cog-bossbot-flunky.webp"
+            alt="Default Image"
+            objectFit="cover"
+        />
+   </div>
    )
 }
